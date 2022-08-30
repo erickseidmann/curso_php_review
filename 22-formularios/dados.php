0bf9898b6d7af3340d128d3b87139771eab19fbd
erickseidmann <erickseidmann@gmail.com>
2022-08-30 11:25:38 -0300

    revisão super global

--- 22-formularios/dados.php
+++ 22-formularios/dados.php
@@ -1,11 +1,19 @@
 <?php
-$nome = $_POST['nome'];
+/*$nome = $_POST['nome'];
 $email = $_POST['email'];
 
 echo"seu nome é $nome e seu email é $email ";
 
-var_dump($_POST);
+var_dump($_POST);*/
 
+/*$nome = $_GET['nome'];
+$email = $_GET['email'];
 
+echo"seu nome é $nome e seu email é $email ";
+
+var_dump($_GET);*/
+
+echo $_GET['idade']."<br>".$_GET['sobrenome'];
+var_dump($_GET);
 
 ?>
--- 22-formularios/index.php
+++ 22-formularios/index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!--<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,7 +10,25 @@
     <form action="dados.php" method="POST">
         Nome: <input type="text" name="nome">
         Email: <input type="email" name="email">
-        <input type="submit" name="enviar">
+        <button type="submit">Enviar</button>
     </form>
+</body>
+</html>-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="dados.php" method="GET">
+        Nome: <input type="text" name="nome">
+        Email: <input type="email" name="email">
+        <button type="submit">Enviar</button>
+    </form>
+    <a href="dados.php?idade=25&sobrenome=Santos">Enviar dados</a>
 </body>
 </html>
