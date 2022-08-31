226f615cdc6f98ff6afe06899fb24fa1abf4c432
erickseidmann <erickseidmann@gmail.com>
2022-08-31 10:59:09 -0300

    validação teste

--- 22-formularios/index.php
+++ 22-formularios/index.php
@@ -20,13 +20,44 @@ FILTER_VALIDATE_URL
     <title>Document</title>
 </head>
 <body>
-    <form action="dados.php" method="POST">
+    <form action="dados.php" method="GET">
         Nome: <input type="text" name="nome">
         Email: <input type="email" name="email">
         <button type="submit">Enviar</button>
     </form>
 </body>
 </html>-->
+<?php
+if(isset($_POST['enviar-formulario'])):
+    //array de erro
+    $erros = array();
+
+    //validações
+    if(!$idade = filter_input(INPUT_POST, 'idade', FILTER_VALIDATE_INT)):
+        $erros[] = "desculpe, idade precisa ser uma idade valida";
+    endif;
+    if(!$idade = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL)):
+        $erros[] = "Precisa ser um email valido";
+    endif;
+    if(!$idade = filter_input(INPUT_POST, 'peso', FILTER_VALIDATE_FLOAT)):
+        $erros[] = "Precisa ser um peso valido";
+    endif;
+    if(!$idade = filter_input(INPUT_POST, 'ip', FILTER_VALIDATE_IP)):
+        $erros[] = "Precisa ser um ip valido";
+    endif;
+    if(!$idade = filter_input(INPUT_POST, 'url', FILTER_VALIDATE_URL)):
+        $erros[] = "Precisa ser uma url valido";
+    endif;
+    
+    if(!empty($erros)):
+        foreach($erros as $erro):
+            echo"<li> $erro <li>";
+        endforeach;
+    else:
+        echo "Parabens, seus dados estão corretos! ";
+    endif;
+endif;
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -37,11 +68,13 @@ FILTER_VALIDATE_URL
     <title>Document</title>
 </head>
 <body>
-    <form action="dados.php" method="GET">
-        Nome: <input type="text" name="nome">
-        Email: <input type="email" name="email">
-        <button type="submit">Enviar</button>
+    <form action="<?php echo $_SERVER['PHP_SELF']?>" method="POST">
+        Idade: <input type="text" name="idade"><br><br>
+        Email: <input type="text" name="email"><br><br>
+        Peso: <input type="text" name="peso"><br><br>
+        IP: <input type="text" name="ip"><br><br>
+        URL: <input type="text" name="url"><br><br>
+        <button type="submit" name="enviar-formulario">Enviar</button><br><br>
     </form>
-    <a href="dados.php?idade=25&sobrenome=Santos">Enviar dados</a>
 </body>
 </html>
